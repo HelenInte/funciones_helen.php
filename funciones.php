08867caf5211f7df8f64d8ade1d3712a0760e250
Helen Maribel Interiano Salguero <elsasalguero@gmail.com>
2026-03-23 13:33:16 -0600

    remplazar texto

--- funciones.php
+++ funciones.php
@@ -7,3 +7,6 @@ echo str_replace("Mundo", "PHP", $texto);
 
 // 1. strlen: Devuelve la longitud de un string (conteo de caracteres).
 $longitud = strlen("Hola"); // 4
+
+// 2. str_replace: Reemplaza todas las apariciones de un texto por otro.
+$texto = str_replace("mundo", "PHP", "Hola mundo"); // "Hola PHP"
