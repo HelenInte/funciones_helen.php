759478017fffcd17926335426a89c43bffdab547
Helen Maribel Interiano Salguero <elsasalguero@gmail.com>
2026-03-23 13:36:36 -0600

    elimina espacios en blanco

--- funciones.php
+++ funciones.php
@@ -5,13 +5,16 @@ echo strlen($texto);
 echo str_replace("Mundo", "PHP", $texto); 
 
 // 1. strlen: Devuelve la longitud de un string (conteo de caracteres).
-$longitud = strlen("Hola"); // 4
+$longitud = strlen("Hola"); 
 
 // 2. str_replace: Reemplaza todas las apariciones de un texto por otro.
-$texto = str_replace("mundo", "PHP", "Hola mundo"); // "Hola PHP"
+$texto = str_replace("mundo", "PHP", "Hola mundo"); 
 
 // 3. strtolower: Convierte toda la cadena a minúsculas.
-$min = strtolower("HOLA"); // "hola"
+$min = strtolower("HOLA"); 
 
 // 4. strtoupper: Convierte toda la cadena a mayúsculas.
-$may = strtoupper("hola"); // "HOLA"
+$may = strtoupper("hola"); 
+
+// 5. trim: Elimina espacios en blanco (u otros caracteres) del inicio y final.
+$limpio = trim("  suave  "); 
