f3d8da194073047123b755d80e25b7a6cfdc6829
Helen Maribel Interiano Salguero <elsasalguero@gmail.com>
2026-03-23 13:34:16 -0600

    combierte todo en minusculas

--- funciones.php
+++ funciones.php
@@ -4,9 +4,11 @@ $texto = "Hola Mundo";
 echo strlen($texto); 
 echo str_replace("Mundo", "PHP", $texto); 
 
-
 // 1. strlen: Devuelve la longitud de un string (conteo de caracteres).
 $longitud = strlen("Hola"); // 4
 
 // 2. str_replace: Reemplaza todas las apariciones de un texto por otro.
 $texto = str_replace("mundo", "PHP", "Hola mundo"); // "Hola PHP"
+
+// 3. strtolower: Convierte toda la cadena a minúsculas.
+$min = strtolower("HOLA"); // "hola"
