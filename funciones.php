02504d4e603c4d6407f9406f88647fa91357a644
Helen Maribel Interiano Salguero <elsasalguero@gmail.com>
2026-03-23 13:32:02 -0600

    conteo de caracteres

--- funciones.php
+++ funciones.php
@@ -4,3 +4,6 @@ $texto = "Hola Mundo";
 echo strlen($texto); 
 echo str_replace("Mundo", "PHP", $texto); 
 
+
+// 1. strlen: Devuelve la longitud de un string (conteo de caracteres).
+$longitud = strlen("Hola"); // 4
