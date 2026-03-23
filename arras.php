573066fd9dc3faff34852b1ff7eeb868d26ed26c
Helen Maribel Interiano Salguero <elsasalguero@gmail.com>
2026-03-23 13:25:02 -0600

    manipulacion de strings

---
+++ arras.php
@@ -0,0 +1,10 @@
+<?php
+$notas = {85, 62, 91,73,88};
+// cobtar
+echo count($notas) ;
+sort($notas);
+array_push($notas, 95);
+// buscar funciones nativas de php meter las funciones un un archivo.php con comentarios ponerle el titulo de cada funcion y explicar que hace cada funcion  las primeras 30 mas importantes
+$ultimo = array_pop($notas);
+$idx = array_search(91, $notas);
+if (in_array)
